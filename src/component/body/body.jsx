63bd30c1a71731a6cd7a5b8c8f6181369ada6f6b
divyanshuan <divyanshuvermaji@gmail.com>
2022-04-19 21:41:54 +0530

    final commit

--- src/component/body/body.jsx
+++ src/component/body/body.jsx
@@ -1,13 +1,13 @@
 import '../styles/body.css';
+import bgVideo from '../../assets/202081.mp4'
 
 function Body() {
   return (
     <div>
       <div className="container">
-                {/* <video className="head_video" autoPlay muted loop>
+            <video className="head_video" autoPlay muted loop>
             <source src={bgVideo} type="video/mp4"/>
-            </video> */}
-         <p>Home</p>
+            </video> 
       </div>
 
     </div>
